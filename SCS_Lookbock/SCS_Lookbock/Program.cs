cfe66fbc29a3008bf3925e9d2b762978716b955c
Florian Hinrichsen <florian@fam-hinrichsen.de>
2019-04-06 10:37:00 +0200

    #: Add global catch.

--- SCS_Lookbock/SCS_Lookbock/Program.cs
+++ SCS_Lookbock/SCS_Lookbock/Program.cs
@@ -16,9 +16,22 @@ namespace SCS_Lookbock
         static void Main()
         {
             log.Info("Application started");
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.ThreadException += Application_ThreadException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainView());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+
+            log.Fatal("Unknown thread exception occured.", e.Exception);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            log.Fatal("Unknown exception occured.", (Exception)e.ExceptionObject);
         }
     }
 }
